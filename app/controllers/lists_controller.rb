35947f530ec4e029fb778cfcd10726eb3fa98249
Stephanie Ramiaramanana <vanesteric1987@gmail.com>
2022-05-27 09:51:45 +0100

    rails watch list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,14 +14,20 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-  # POST /gardens
+  # POST /list
   def create
     @list = List.new(list_params)
 
     if @list.save
-      redirect_to @garden, notice: 'List was successfully created.'
+      redirect_to @list, notice: 'List was successfully created.'
     else
       render :new
     end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
